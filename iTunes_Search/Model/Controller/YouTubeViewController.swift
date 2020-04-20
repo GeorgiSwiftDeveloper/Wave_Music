53de71661a1f3853cbfc10d5148de3aa050ae755
Georgi Malkhasyan <malkhasyangeorge@gmail.com>
2020-04-19 21:33:01 -0700

    YouTubeViewController code cleanup.

--- iTunes_Search/Model/Controller/YouTubeViewController.swift
+++ iTunes_Search/Model/Controller/YouTubeViewController.swift
@@ -19,20 +19,22 @@ class YouTubeViewController: UIViewController, WKNavigationDelegate {
     
     
     var genreTitle: Video?
-    let appGroupName = "br.com.tntstudios.youtubeplayer"
+    var youTubeVideoWebView: WKWebView!
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let urlw = URL(string: "https://www.youtube.com/watch?v=\(genreTitle!.videoId)&index=4&showinfo=0&controls=0&autohide=1" )
-   
-        loadYoutubeIframe(youtubeVideoId: genreTitle!.videoId)
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.allowsInlineMediaPlayback = true
+        webConfiguration.mediaTypesRequiringUserActionForPlayback = []
+        youTubeVideoWebView = WKWebView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 350), configuration: webConfiguration)
+        self.view.addSubview(youTubeVideoWebView)
+        loadYouTubeVideoUrl()
     }
-
     
     
-    func loadYoutubeIframe(youtubeVideoId: String) {
-        
+    func loadYouTubeVideoUrl(){
         let html =
             "<html>" +
                 "<body style='margin: 0;'>" +
@@ -49,16 +51,11 @@ class YouTubeViewController: UIViewController, WKNavigationDelegate {
                 "   }" +
                 "</script>" +
                 "<div style='justify-content: center; align-items: center; display: flex; height: 100%;'>" +
-                "   <iframe id='playerId' type='text/html' width='100%' height='100%' src='https://www.youtube.com/embed/\(youtubeVideoId)?" +
-                "enablejsapi=1&rel=0&playsinline=0&autoplay=0&showinfo=0&modestbranding=1' frameborder='0'>" +
+                "<iframe id='playerId' type='text/html' width='100%' height='100%' src='https://www.youtube.com/embed/\(genreTitle!.videoId)?" +
+                "enablejsapi=1&rel=0&playsinline=1&autoplay=0&showinfo=0&modestbranding=1' frameborder='0'>" +
                 "</div>" +
                 "</body>" +
         "</html>"
-        
-        loadYouTubeVideo?.loadHTMLString(html, baseURL:  URL(string: "http://www.youtube.com"))
+        youTubeVideoWebView.loadHTMLString(html, baseURL: URL(string: "http://www.youtube.com"))
     }
-    
-    
-    
-    
 }
