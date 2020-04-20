e6f650d22b120a37c0c746389e7b02a923cb972e
Georgi Malkhasyan <malkhasyangeorge@gmail.com>
2020-04-19 20:39:41 -0700

    Play YouTube video with  videoId (WKWebView)

--- iTunes_Search/Model/Controller/YouTubeViewController.swift
+++ iTunes_Search/Model/Controller/YouTubeViewController.swift
@@ -10,38 +10,55 @@ import UIKit
 import YouTubePlayer
 import AVFoundation
 import AVKit
-class YouTubeViewController: UIViewController {
-
-    
-    @IBOutlet weak var loadYouTubeVideo: UIWebView!
+import WebKit
+class YouTubeViewController: UIViewController, WKNavigationDelegate {
     
     
-     var genreTitle: Video?
-  var player = AVPlayer()
-  var playerLayer = AVPlayerViewController()
+    @IBOutlet weak var loadYouTubeVideo: WKWebView?
     
+    
+    
+    var genreTitle: Video?
+    let appGroupName = "br.com.tntstudios.youtubeplayer"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlw = URL(string: "https://www.youtube.com/watch?v=\(genreTitle!.videoId)=\(genreTitle!.videoPlaylistId)&index=4" )
-        let urlRequest = URLRequest(url: urlw!)
-//                    loadYouTubeVideo.loadVideoURL(url as URL)
-
-        loadYouTubeVideo.loadRequest(urlRequest)
-        loadYouTubeVideo.scrollView.isScrollEnabled = false
-        //        self.player = AVPlayer(url: urlw!)
-//        self.playerLayer.player = self.player
+//        let urlw = URL(string: "https://www.youtube.com/watch?v=\(genreTitle!.videoId)&index=4&showinfo=0&controls=0&autohide=1" )
+   
+        loadYoutubeIframe(youtubeVideoId: genreTitle!.videoId)
     }
 
     
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-//        playerLayer.frame = loadYouTubeVideo.bounds
+    func loadYoutubeIframe(youtubeVideoId: String) {
+        
+        let html =
+            "<html>" +
+                "<body style='margin: 0;'>" +
+                "<script type='text/javascript' src='http://www.youtube.com/iframe_api'></script>" +
+                "<script type='text/javascript'> " +
+                "   function onYouTubeIframeAPIReady() {" +
+                "      ytplayer = new YT.Player('playerId',{events:{'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}}) " +
+                "   } " +
+                "   function onPlayerReady(a) {" +
+                "       window.location = 'br.com.tntstudios.youtubeplayer://state=6'; " +
+                "   }" +
+                "   function onPlayerStateChange(d) {" +
+                "       window.location = 'br.com.tntstudios.youtubeplayer://state=' + d.data; " +
+                "   }" +
+                "</script>" +
+                "<div style='justify-content: center; align-items: center; display: flex; height: 100%;'>" +
+                "   <iframe id='playerId' type='text/html' width='100%' height='100%' src='https://www.youtube.com/embed/\(youtubeVideoId)?" +
+                "enablejsapi=1&rel=0&playsinline=0&autoplay=0&showinfo=0&modestbranding=1' frameborder='0'>" +
+                "</div>" +
+                "</body>" +
+        "</html>"
+        
+        loadYouTubeVideo?.loadHTMLString(html, baseURL:  URL(string: "http://www.youtube.com"))
     }
-
     
-
-
+    
+    
+    
 }
