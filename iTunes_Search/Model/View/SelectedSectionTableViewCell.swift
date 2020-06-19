515aa9822cf47471491441fa8352c5975c89bdb6
Georgi Malkhasyan <malkhasyangeorge@gmail.com>
2020-06-19 15:27:35 -0700

    Code Improving

--- iTunes_Search/Model/View/SelectedSectionTableViewCell.swift
+++ iTunes_Search/Model/View/SelectedSectionTableViewCell.swift
@@ -17,84 +17,44 @@ class SelectedSectionTableViewCell: UITableViewCell {
     var videoImageUrl = String()
     
     func configureTopHitsCell(_ video: Video){
-        DispatchQueue.main.async {
-            self.videoID = video.videoId ?? ""
-            self.videoImageUrl = video.videoImageUrl ?? ""
-            self.topHitLabelText.text = video.videoTitle
-            if video.videoImageUrl != ""{
-            let imageUrl = URL(string: video.videoImageUrl ?? "")
-            do{
-                let data:NSData = try NSData(contentsOf: imageUrl!)
-                self.topHitImageView.image =  UIImage(data: data as Data)
-                
-            }catch{
-                print("Image data not found")
-                }
-            }
-            self.topHitImageView.layer.borderWidth = 3
-            self.topHitImageView.layer.masksToBounds = false
-            self.topHitImageView.layer.borderColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
-            self.topHitImageView.layer.shadowOpacity = 2
-            self.topHitImageView.layer.shadowPath = UIBezierPath(rect: self.topHitImageView.bounds).cgPath
-            self.topHitImageView.layer.cornerRadius = self.topHitImageView.frame.height/2
-            self.topHitImageView.layer.shadowRadius = 3
-            self.topHitImageView.layer.shadowOffset = .zero
-            self.topHitImageView.clipsToBounds = true
-        }
+        configureMainVideoCell(video)
     }
     
     func configureMyLibraryCell(_ video: Video){
-        DispatchQueue.main.async {
-            self.topHitLabelText.text = video.videoTitle
-            if video.videoImageUrl != ""{
-            let imageUrl = URL(string: video.videoImageUrl ?? "")
-            do{
-                let data:NSData = try NSData(contentsOf: imageUrl!)
-                self.topHitImageView.image =  UIImage(data: data as Data)
-                
-            }catch{
-                print("Image data not found")
-                }
-            }
-            self.topHitImageView.layer.borderWidth = 3
-            self.topHitImageView.layer.masksToBounds = false
-            self.topHitImageView.layer.borderColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
-            self.topHitImageView.layer.shadowOpacity = 2
-            self.topHitImageView.layer.shadowPath = UIBezierPath(rect: self.topHitImageView.bounds).cgPath
-            self.topHitImageView.layer.cornerRadius = self.topHitImageView.frame.height/2
-            self.topHitImageView.layer.shadowRadius = 3
-            self.topHitImageView.layer.shadowOffset = .zero
-            self.topHitImageView.clipsToBounds = true
-        }
+       configureMainVideoCell(video)
     }
     
     
     func configureRecentlyPlayedCell(_ video: Video){
-          self.videoImageUrl = video.videoImageUrl ?? ""
-          self.videoID = video.videoId ?? ""
-          DispatchQueue.main.async {
-              self.topHitLabelText.text = video.videoTitle
-                
-              if video.videoImageUrl != ""{
-              let imageUrl = URL(string: video.videoImageUrl ?? "")
-              do{
-                  let data:NSData = try NSData(contentsOf: imageUrl!)
-                  self.topHitImageView.image =  UIImage(data: data as Data)
-                  
-              }catch{
-                  print("Image data not found")
-                  }
-              }
-              self.topHitImageView.layer.borderWidth = 3
-              self.topHitImageView.layer.masksToBounds = false
-              self.topHitImageView.layer.borderColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
-              self.topHitImageView.layer.shadowOpacity = 2
-              self.topHitImageView.layer.shadowPath = UIBezierPath(rect: self.topHitImageView.bounds).cgPath
-              self.topHitImageView.layer.cornerRadius = self.topHitImageView.frame.height/2
-              self.topHitImageView.layer.shadowRadius = 3
-              self.topHitImageView.layer.shadowOffset = .zero
-              self.topHitImageView.clipsToBounds = true
-          }
+          configureMainVideoCell(video)
       }
+    
+    func configureMainVideoCell(_ video: Video){
+        self.videoImageUrl = video.videoImageUrl ?? ""
+        self.videoID = video.videoId ?? ""
+        DispatchQueue.main.async {
+            self.topHitLabelText.text = video.videoTitle
+              
+            if video.videoImageUrl != ""{
+            let imageUrl = URL(string: video.videoImageUrl ?? "")
+            do{
+                let data:NSData = try NSData(contentsOf: imageUrl!)
+                self.topHitImageView.image =  UIImage(data: data as Data)
+                
+            }catch{
+                print("Image data not found")
+                }
+            }
+            self.topHitImageView.layer.borderWidth = 3
+            self.topHitImageView.layer.masksToBounds = false
+            self.topHitImageView.layer.borderColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+            self.topHitImageView.layer.shadowOpacity = 2
+            self.topHitImageView.layer.shadowPath = UIBezierPath(rect: self.topHitImageView.bounds).cgPath
+            self.topHitImageView.layer.cornerRadius = self.topHitImageView.frame.height/2
+            self.topHitImageView.layer.shadowRadius = 3
+            self.topHitImageView.layer.shadowOffset = .zero
+            self.topHitImageView.clipsToBounds = true
+        }
+    }
     
 }
