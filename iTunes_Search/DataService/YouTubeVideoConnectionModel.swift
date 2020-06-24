ae89185ee0f70bdbb0829403735b44fe84ebbad5
Georgi Malkhasyan <malkhasyangeorge@gmail.com>
2020-06-24 11:31:05 -0700

    Code cleanup

--- iTunes_Search/DataService/YouTubeVideoConnectionModel.swift
+++ iTunes_Search/DataService/YouTubeVideoConnectionModel.swift
@@ -60,32 +60,6 @@ class  YouTubeVideoConnection {
             default:
                 break
             }
-        case "YouTubeViewController":
-            API_KEY = "AIzaSyDnZJailNum2kVdCTUPpK80O8ERYBqbnX4"
-            switch genreType {
-            case "Rap":
-                UPLOADS_PLAYLIST_ID = "PL-FVH5VWgRPHNz24zZ5_FLHQWoidN6O1d"
-            case "Hip-Hop":
-                UPLOADS_PLAYLIST_ID = "PLRZlMhcYkA2EQSwaeOeZnJ1nOfpZTsMuS"
-            case "Pop":
-                UPLOADS_PLAYLIST_ID = "PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10"
-            case "Classic Rock":
-                UPLOADS_PLAYLIST_ID = "PLNxOe-buLm6cz8UQ-hyG1nm3RTNBUBv3K"
-            case "R&B":
-                UPLOADS_PLAYLIST_ID = "PLplXQ2cg9B_okySP1Z7MTETWHRKCj1fJ-"
-            case "Dance":
-                UPLOADS_PLAYLIST_ID = "PLWlTX25IDqIwqowTsJmGhqxUWU_6qgG1W"
-            case "Electronic":
-                UPLOADS_PLAYLIST_ID = "PLw6eTMMKY24QLYfmrU2rB8x-lP5Fas2dY"
-            case "Jazz":
-                UPLOADS_PLAYLIST_ID = "PLGBuKfnErZlC8nMxwcNf7oaXhGfAnTwgt"
-            case "Instrumental":
-                UPLOADS_PLAYLIST_ID = "RDQMWJ8bhfD-OBA"
-            case "Blues":
-                UPLOADS_PLAYLIST_ID = "PL5NLp0DgIm9etmf1l7LdyyYQSGPUPngcB"
-            default:
-                break
-            }
         default:
             break
         }
